d65bdb46d1f319495e8fefabd4f07a9fbe40cc8b
ocslegna <gabrielscotillo@gmail.com>
2017-08-04 22:20:48 -0300

    versioning 1.0.7

--- auto_py_torrent/__version__.py
+++ auto_py_torrent/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'auto_py_torrent'
 __description__ = 'Automated tool for file downloads, using torrent or magnet.'
 __url__ = 'https://github.com/ocslegna/auto_py_torrent'
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 __build__ = 0x000111
 __author__ = 'Gabriel Scotillo'
 __author_email__ = 'gabrielscotillo@gmail.com'
